dee30c8df842a60f154e4ada311a4885bdd507bd
sujal <sujalpatel7574@gmail.com>
2023-08-11 18:30:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 from flask import Flask, request, render_template, make_response
 import bypasser
-import threading
 import re
 import os
-from texts import gdrivetext, otherstext, ddltext, shortnertext, HELP_TEXT  
 import pyrogram
-from pyrogram import Client,filters
-from pyrogram.types import InlineKeyboardMarkup,InlineKeyboardButton
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from os import environ, remove
 from threading import Thread
 from json import load
-from re import search
+from texts import gdrivetext, otherstext, ddltext, shortnertext, HELP_TEXT
 from texts import HELP_TEXT
 from ddl import ddllist, direct_link_generator
 from time import time
@@ -87,5 +85,5 @@ def index():
     return render_template("index.html", result=None, prev_links=shortened_links.split(",") if shortened_links else None)
 
 if __name__ == "__main__":
-    print("Web Application Starting")
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
