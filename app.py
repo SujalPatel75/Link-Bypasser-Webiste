999e1f4aa8a3566c1ebfa378d67a6bff7842ba70
SujalPatel <sujalpatel7574@gmail.com>
2023-08-11 13:19:44 +0530

    Added cookies chnages

--- app.py
+++ app.py
@@ -59,14 +59,32 @@ def index():
     if request.method == "POST":
         url = request.form.get("url")
         result = loop_thread(url)
+        
+        # Get previously shortened links from the cookie
+        shortened_links = request.cookies.get('shortened_links')
+        if shortened_links:
+            prev_links = shortened_links.split(',')
+        else:
+            prev_links = []
 
-        resp = make_response(render_template("index.html", result=result))
-        resp.set_cookie('shortened_links', result)
+        # Add the newly shortened link to the list of previously shortened links
+        if result:
+            prev_links.append(result)
+            # Limit the list to a certain number of links if desired
+            if len(prev_links) > 10:  # Adjust the number as needed
+                prev_links = prev_links[-10:]  # Keep the last 10 links
+
+        # Join the list of links back into a comma-separated string
+        shortened_links_str = ','.join(prev_links)
+
+        # Set the updated shortened links in the cookie
+        resp = make_response(render_template("index.html", result=result, prev_links=prev_links))
+        resp.set_cookie('shortened_links', shortened_links_str)
 
         return resp
 
     shortened_links = request.cookies.get('shortened_links')
-    return render_template("index.html", result=None, shortened_links=shortened_links.split(",") if shortened_links else None)
+    return render_template("index.html", result=None, prev_links=shortened_links.split(",") if shortened_links else None)
 
 if __name__ == "__main__":
     print("Web Application Starting")
